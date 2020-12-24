189ad3a9b659273e198218c1d876ca1b20ce7175
Andy <musictopia2@hotmail.com>
2020-12-24 07:14:40 -0700

    Added extension to register the resize listener which is needed for the media queries to work.

---
+++ Components/MediaQueries/ResizeHelpers/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+namespace BasicBlazorLibrary.Components.MediaQueries.ResizeHelpers
+{
+    public class Extensions
+    {
+        public static IServiceCollection RegisterResizeListener(IServiceCollection services)
+        {
+            services.AddScoped<IResizeListener, ResizeListener>();
+            return services; //so i can chain things together.  i like that idea.
+        }
+    }
+}
