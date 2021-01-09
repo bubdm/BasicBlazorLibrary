0da37cf4b8b254daa84862f9dcd34ef7bacca9dd
Andy <musictopia2@hotmail.com>
2021-01-09 06:54:13 -0700

    Delay scrolling for listener to help with performance problems.

--- wwwroot/scrolllistener.js
+++ wwwroot/scrolllistener.js
@@ -1,8 +1,13 @@
 ﻿export function start(dotnetRef, element) {
-    if (element == undefined || element == null) {
-        return;
-    }
-    element.addEventListener('scroll', function () {
-        dotnetRef.invokeMethodAsync('ScrollChanged', element.scrollTop);
-    });
+	if (element == undefined || element == null) {
+		return;
+	}
+	var isScrolling;
+	element.addEventListener('scroll', function () {
+		clearTimeout(isScrolling);
+		isScrolling = setTimeout(function () {
+
+			dotnetRef.invokeMethodAsync('ScrollChanged', element.scrollTop);
+		}, 66);
+	}, false);
 }
