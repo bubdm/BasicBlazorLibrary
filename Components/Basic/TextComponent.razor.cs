d0d85980e2f0c790b328b258a8b5cf282b674127
Andy <musictopia2@hotmail.com>
2021-01-18 09:31:08 -0700

    Created New Text Component. This is a simple one when we don't need any validations and am not submitting any forms.

---
+++ Components/Basic/TextComponent.razor.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+namespace BasicBlazorLibrary.Components.Basic
+{
+    /// <summary>
+    /// this is used for cases where you want a simple text component.  however, does not have any validations.
+    /// </summary>
+    public partial class TextComponent
+    {
+        [Parameter]
+        public string Value { get; set; } = "";
+        [Parameter]
+        public EventCallback<string> ValueChanged { get; set; }
+        //this does not tie to any specific form.
+
+        [Parameter]
+        public string Class { get; set; } = "";
+        [Parameter]
+        public string Style { get; set; } = "";
+        //this is oninput always though.
+
+        private string CurrentValue
+        {
+            get => Value;
+            set
+            {
+                var hasChanged = !EqualityComparer<string>.Default.Equals(value, Value);
+                if (hasChanged)
+                {
+                    ValueChanged.InvokeAsync(value);
+                }
+            }
+        }
+
+    }
+}
