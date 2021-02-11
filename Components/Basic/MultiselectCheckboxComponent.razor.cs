fa6fcbeebe6a8da6d2070452e0ae3902dd0fa473
Andy <musictopia2@hotmail.com>
2021-02-11 09:43:45 -0700

    Added possibility of using images to show checked and unchecked.

--- Components/Basic/MultiselectCheckboxComponent.razor.cs
+++ Components/Basic/MultiselectCheckboxComponent.razor.cs
@@ -4,11 +4,20 @@ using Microsoft.AspNetCore.Components;
 using System;
 using System.Linq;
 using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
 using aa = BasicBlazorLibrary.Components.CssGrids.Helpers;
 namespace BasicBlazorLibrary.Components.Basic
 {
     public partial class MultiselectCheckboxComponent<TValue>
     {
+
+
+        [Parameter]
+        public string CheckBoxedImage { get; set; } = "";
+        [Parameter]
+        public string UncheckedImage { get; set; } = "";
+        //if they are specified, then use image instead of 
+
         private static string GetColumns => aa.RepeatMaximum(2);
         [Parameter]
         public CustomBasicList<TValue> Data { get; set; } = new();
@@ -18,6 +27,38 @@ namespace BasicBlazorLibrary.Components.Basic
         public Func<TValue, string>? RetrieveValue { get; set; }
         [Parameter]
         public EventCallback<CustomBasicList<TValue>> ValuesChanged { get; set; } //this is for data binding.
+
+
+        private enum EnumState
+        {
+            Old,
+            Images,
+            NotValid
+        }
+        private EnumState _state;
+        //private bool _valid;
+
+        protected override void OnParametersSet()
+        { 
+            if (string.IsNullOrWhiteSpace(CheckBoxedImage) && string.IsNullOrWhiteSpace(UncheckedImage))
+            {
+                _state = EnumState.Old;
+                return;
+            }
+            if (string.IsNullOrEmpty(CheckBoxedImage) && string.IsNullOrWhiteSpace(UncheckedImage) == false)
+            {
+                _state = EnumState.NotValid;
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(UncheckedImage) && string.IsNullOrWhiteSpace(CheckBoxedImage) == false)
+            {
+                _state = EnumState.NotValid;
+                return;
+            }
+            _state = EnumState.Images;
+
+        }
+
         private bool IsSelected(TValue info)
         {
             return Values.Any(xxx => xxx!.Equals(info));
