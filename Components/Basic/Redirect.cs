e95ab3897a5cabc7b7fe5affaeb0cdac118b142c
Andy <musictopia2@hotmail.com>
2021-02-21 06:42:11 -0700

    Added classes for navigation to urls.

---
+++ Components/Basic/Redirect.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+namespace BasicBlazorLibrary.Components.Basic
+{
+    public class Redirect : ComponentBase
+    {
+        [Inject]
+        private NavigationManager? Navigates { get; set; }
+        [Parameter]
+        public string Url { get; set; } = "";
+        protected override void OnInitialized()
+        {
+            Navigates!.NavigateTo(Url);
+        }
+
+    }
+}
